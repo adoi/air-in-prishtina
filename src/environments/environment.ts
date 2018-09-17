d01b3a01ae79220754efe3bea344eaef1ca0b05c
adoi <adonismurati@gmail.com>
2018-09-17 07:44:13 +0200

    Add the api key to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'https:/api.airvisual.com/v2/city?city=Pristina&state=Pristina&country=Kosovo&key=jQ9jLuPyuT74N3PA'
 };
 
 /*
